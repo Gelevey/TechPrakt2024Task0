c1a91783550f173a2d8278e7ad151fcbaaf58064
Гелевей Олексій <ipz224_goa@student.ztu.edu.ua>
2024-02-03 18:53:49 +0200

    Created Task_Calculate_Two_Numbers

---
+++ Practice/Task_Calculate_Two_Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_Calculate_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 10, b = 10;
+
+            Console.WriteLine(a+b);
+        }
+    }
+}
